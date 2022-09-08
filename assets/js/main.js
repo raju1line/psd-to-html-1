ade8ea8ad9546973d6fd8c7aa73e83d1a04d55a0
raju1line <raju1line@gmail.com>
2022-09-08 10:21:49 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(".case-study-carousel").owlCarousel({
+    loop: true,
+    margin: 34,
+    nav: false,
+    dots: true,
+    items: 3
+
+});
+$(".testmonial-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: true,
+    items: 1
+});
+$(".logo-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    items:6
+});
