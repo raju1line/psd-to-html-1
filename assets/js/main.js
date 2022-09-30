bd89fb1d899541e0e82dcb46cf90ceb10994d13f
raju1line <raju1line@gmail.com>
2022-09-30 11:35:10 +0600

    complie home page-3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,46 @@
 $(".case-study-carousel").owlCarousel({
-    loop: true,
-    margin: 34,
-    nav: false,
-    dots: true,
-    items: 3
-
+  loop: true,
+  margin: 34,
+  nav: false,
+  dots: true,
+  items: 3,
+});
+$(".case-study-carousel2").owlCarousel({
+  loop: true,
+  margin: 34,
+  nav: false,
+  dots: true,
+  items: 5,
 });
 $(".testmonial-carousel").owlCarousel({
-    loop: true,
-    margin: 30,
-    nav: false,
-    dots: true,
-    items: 1
+  loop: true,
+  margin: 30,
+  nav: false,
+  dots: true,
+  items: 1,
 });
 $(".logo-carousel").owlCarousel({
-    loop: true,
-    margin: 30,
-    nav: false,
-    dots: false,
-    items:6
+  loop: true,
+  margin: 30,
+  nav: false,
+  dots: false,
+  items: 6,
+});
+$(".homepage-slide").owlCarousel({
+  loop: true,
+  nav: true,
+  navText: [
+    "<i class='zmdi zmdi-arrow-left'></i>",
+    "<i class='zmdi zmdi-arrow-right'></i>",
+  ],
+  dots: true,
+  items: 1,
+});
+$(".menu-tigger").on("click", function () {
+  $(".off-canvas ").addClass("show-off-canvas");
+  $(".off-canvas-menu-shade ").addClass("active");
+});
+$(".menue-close, .off-canvas-menu-shade ").on("click", function () {
+  $(".off-canvas ").removeClass("show-off-canvas");
+  $(".off-canvas-menu-shade ").removeClass("active");
 });
