e0134ad590cf876f7cffe977ea74fbfa811e4b75
raju1line <raju1line@gmail.com>
2022-10-01 19:39:13 +0600

    complite page4

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,43 @@ $(".menue-close, .off-canvas-menu-shade ").on("click", function () {
   $(".off-canvas ").removeClass("show-off-canvas");
   $(".off-canvas-menu-shade ").removeClass("active");
 });
+$(".single-testimonial-box").hover(function () {
+  $(".single-testimonial-box").removeClass("active");
+  $(this).addClass("active");
+});
+$(".video-btn").magnificPopup({
+  type: "iframe",
+});
+// $(document).ready(function () {
+//   $(".video-btn").magnificPopup({
+//     type: "iframe",
+//     iframe: {
+//       markup:
+//         '<div class="mfp-iframe-scaler">' +
+//         '<div class="mfp-close"></div>' +
+//         '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+//         "</div>",
+
+//       patterns: {
+//         youtube: {
+//           index: "youtube.com/",
+
+//           id: "v=",
+
+//           src: "https://www.youtube.com/embed/%id%?autoplay=1",
+//         },
+//         vimeo: {
+//           index: "vimeo.com/",
+//           id: "/",
+//           src: "//player.vimeo.com/video/%id%?autoplay=1",
+//         },
+//         gmaps: {
+//           index: "//maps.google.",
+//           src: "%id%&output=embed",
+//         },
+//       },
+
+//       srcAction: "iframe_src",
+//     },
+//   });
+// });
